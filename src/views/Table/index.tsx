71be65039056625981d82a40797952e693fbe868
yanghang1 <yanghang1@ezbuy.com>
2019-04-03 11:34:07 +0800

    Introduce antd

---
+++ src/views/Table/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Button } from "antd"
+export default class LoginPage extends React.Component<{}, {}> {
+	render() {
+		return (
+			// <h1>ni hao a</h1>
+			<Button type="danger">hi</Button>
+		)
+	}
+}
